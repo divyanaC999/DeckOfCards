5013cbce53d23f098fe5b6148c86da16aaf7c6c8
Divya Chourasiya <93827030+divyanaC999@users.noreply.github.com>
2022-03-07 08:49:52 +0530

    delete DeckOfCardsMain.java

--- src/com/bridgelabz/workshop2/DeckOfCardsMain.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.workshop2;
-
-public class DeckOfCardsMain {
-	public static void main(String[] args) {
-		DeckOfCards deckOfCardsGame = new DeckOfCards();
-		deckOfCardsGame.welcome();
-		deckOfCardsGame.deckOfCards();
-	}
-}
